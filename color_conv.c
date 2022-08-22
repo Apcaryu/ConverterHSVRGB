30c81c923d84ea00226caf62c6b52a0289f73b4e
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-22 16:06:25 +0200

    make int_to_rgb function

---
+++ color_conv.c
@@ -0,0 +1,21 @@
+#include "color_conv.h"
+
+t_rgb	hsv_to_rgb(t_hsv hsv_color)
+{}
+
+t_hsv	rgb_to_hsv(t_rgb rgb_color)
+{}
+
+int		rgb_to_int(t_rgb rgb_color)
+{}
+
+t_rgb	int_to_rgb(int int_color)
+{
+	t_rgb 			color_out;
+	
+	color_out.r = (int)(int_color / (256^2));
+	color_out.g = (int)((int_color / 256) % 256);
+	color_out.b = int_color % 256;
+
+	return(color_out);
+}
