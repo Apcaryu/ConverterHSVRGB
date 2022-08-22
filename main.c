38251db78f269416d6c7733504075f5c2c3d9a0a
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-08-22 16:19:09 +0200

    add test for rgb_to_int

--- main.c
+++ main.c
@@ -10,4 +10,5 @@ int	main(void)
 	color = 16711935;
 	rgb_color = int_to_rgb(color);
 	printf("r: %u | g: %u | b: %u\n", rgb_color.r, rgb_color.g, rgb_color.b);
+	printf("int = %u", rgb_to_int(rgb_color));
 }
